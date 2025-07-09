2dce4b13731785d5fb235171220bb245a177a4ce
rianMuhammadA <rianmuhammadafriansyah@gmail.com>
2025-07-09 14:41:47 +0700

    mempelajari dan mengimplementasikan prototype yang ada di function declaration ~ WPU

--- Latihan_Js_Lanjutan/script.js
+++ Latihan_Js_Lanjutan/script.js
@@ -83,33 +83,97 @@
 
 // 4. Object.create
 
-const methondMahasiswa = {
+// const methondMahasiswa = {
 
-    makan: function(porsi) {
-        this.energy += porsi;
-        console.log(`halo ${this.nama}, selamat makan! `);
-    },
+//     makan: function(porsi) {
+//         this.energy += porsi;
+//         console.log(`halo ${this.nama}, selamat makan! `);
+//     },
 
-    main: function (jam) {
-        this.energy -= jam
-        console.log(`Halo ${this.nama}, selamat bermain!`) 
-     },
+//     main: function (jam) {
+//         this.energy -= jam
+//         console.log(`Halo ${this.nama}, selamat bermain!`) 
+//      },
 
-    tidur: function (jam) {
-        this.energy += jam*2
-        console.log(`Halo ${this.nama}, selamat tidur!`) 
-     }
-}
+//     tidur: function (jam) {
+//         this.energy += jam*2
+//         console.log(`Halo ${this.nama}, selamat tidur!`) 
+//      }
+// }
+
+// function Mahasiswa(nama, energy){
+//     let mahasiswa = Object.create(methondMahasiswa)
+//     mahasiswa.nama = nama
+//     mahasiswa.energy = energy
+
+
+
+//     return mahasiswa
+// }
+
+// let rian = Mahasiswa('rian', 10)
+// let dian = Mahasiswa('dian', 10)
+
+// membuat kode di atas lebih efektif lagi dengan memanfaatkan PROTOTYPE
 
 function Mahasiswa(nama, energy){
-    let mahasiswa = Object.create(methondMahasiswa)
-    mahasiswa.nama = nama
-    mahasiswa.energy = energy
+    // let mahasiswa = Object.create(methondMahasiswa)
+    // let mahasiswa = {}
 
-
-
-    return mahasiswa
+    // yang terjadi dibelakan layar apabila menggunakan constructor function sudah terinput prototype
+    // jadi code berikut sudah otomatis tertulis
+    // let this = Object.create(Mahasiswa.prototype); *dan di paling akhir fungsinya juga tertulis* return this;
+    this.nama = nama
+    this.energy = energy
+    // return mahasiswa
 }
 
-let rian = Mahasiswa('rian', 10)
-let dian = Mahasiswa('dian', 10)
+Mahasiswa.prototype.makan = function(porsi){
+    this.energy += porsi
+    return `halo ${this.nama}, selamat makan! `
+}
+Mahasiswa.prototype.main = function(jam){
+    this.energy -= jam
+    return `halo ${this.nama}, selamat main! `
+}
+Mahasiswa.prototype.tidur = function(jam){
+    this.energy += jam*2
+    return `halo ${this.nama}, selamat tidur! `
+}
+
+let rian = new Mahasiswa('Rian',40)
+
+// versi Class
+
+// class Mahasiswa {
+//     constructor(nama,energy){
+//         this.nama = nama
+//         this.energy = energy
+//     }
+//     makan(porsi){
+//     this.energy += porsi
+//     return `halo ${this.nama}, selamat makan! `
+//     }
+//     main(jam){
+//         this.energy -= jam
+//         return `halo ${this.nama}, selamat main! `
+//     }
+//     tidur(jam){
+//         this.energy += jam*2
+//         return `halo ${this.nama}, selamat tidur! `
+//     }
+// }
+
+// let rian = new Mahasiswa('rian', 20)
+
+
+// apabila kita memberikan perintah  "let angka = []" sebenarnya yang terjadi dibelakang layar seperti di bawah ini
+// 
+// let angka = new Array{};
+// dan 'new Array{}' di atas otomatis memiliki fungsi berikut
+// function Array(){
+//     let this = Object.create(Array.prorotype)
+// }
+
+let angka = [2,1,3,4,5]
+console.log(angka);
