daafb34bffaeba1adce1062eac2798c57f367508
rianMuhammadA <rianmuhammadafriansyah@gmail.com>
2025-07-10 11:48:23 +0700

    latihan penerapan template literal ~ WPU

--- Latihan_Js_Lanjutan/templateLiteral.js
+++ Latihan_Js_Lanjutan/templateLiteral.js
@@ -52,4 +52,121 @@
 //     email : 'Rian@gmail.com'
 // }
 
-// const el = ``
+// const el = `<div>${expression}</div>`
+
+
+
+
+
+
+// const mhs = {
+//     nama : 'Rian Muhammad',
+//     umur : 20,
+//     nrp : '218479132',
+//     email : 'Rian@gmail.com'
+// }
+
+
+// const el = `
+// <div className="mhs">
+//     <h2 className=""> ${mhs.nama} </h2>
+//     <span className=""> ${mhs.nrp}</span>
+// </div>
+// `
+
+// // console.log(el);
+
+// document.body.innerHTML = el
+
+
+
+// looping
+
+// const mhs = [
+//     {
+//     nama : 'rian',
+//     email : 'rian@gmail.com'
+// },
+//     {
+//     nama : 'rianisa',
+//     email : 'rianisa@gmail.com'
+// },
+//     {
+//     nama : 'kurni',
+//     email : 'kurni@gmail.com'
+// }
+// ]
+
+// const el = `
+// <div className="mhs">
+//     <p className=""> ${mhs.map(m => `<ul>
+//         <li>${m.nama}</li>
+//         <li>${m.nrp}</li>
+//     </ul>`).join(' ')} </p>
+// </div>
+// `
+
+// document.body.innerHTML = el
+
+
+
+
+// Conditional
+// ternary
+
+// const lagu = {
+//     judul: "sepatu",
+//     penyanyi: 'tulus',
+//     feat: 'rendy'
+// }
+
+// const el = `
+// <div className="lagu">
+//   <ul>
+//     <li>${lagu.penyanyi}</li>
+//     <li>${lagu.judul} ${lagu.feat ? ` (feat. ${lagu.feat})` : ''}</li>
+//   </ul>
+// </div>
+// `
+
+
+// document.body.innerHTML = el
+
+
+
+
+
+
+
+// Nested
+// HTML Fragment bersarang
+const mhs = {
+    nama : 'Rian Muhammad',
+    semester: 5,
+    matKul: [
+        'rekayasa web',
+        'analisis',
+        'PSI',
+        'PBO'
+    ]
+
+}
+function cetakMatakuliah(matKul){
+    return `
+    <ol>
+        ${matKul.map(mk => `<li className="">${mk}</li>`).join('')}
+    </ol>
+    `
+}
+
+const el = `
+<div className="mhs">
+ <h2 className="">${mhs.nama}</h2>
+ <span className="semester">Semester : ${mhs.semester}</span>
+ <h4 className="matkul">Mata Kuliah</h4>
+ ${cetakMatakuliah(mhs.matKul)}
+</div>
+`
+
+
+document.body.innerHTML = el
