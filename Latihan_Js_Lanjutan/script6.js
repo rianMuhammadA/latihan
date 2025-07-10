69e9bf1c53f4728b25b2ba22afdb6eb41e48bb54
rianMuhammadA <rianmuhammadafriansyah@gmail.com>
2025-07-10 09:31:59 +0700

    mempraktekan teori Higher Order Function dengan fungsi filter(), map() & reduce(). serta mengaplikasikan method chain ~ WPU

--- Latihan_Js_Lanjutan/script6.js
+++ Latihan_Js_Lanjutan/script6.js
@@ -43,7 +43,48 @@
 
 
 
-// Contoh Higher Order Function yang sering digunakan untuk pengelolaan 
+// Contoh Higher Order Function yang sering digunakan untuk pengelolaan [filter, map & reduce]
 // - Array.prototypr.map()
 // - Array.prototypr.filter()
 // - Array.prototypr.reduce()
+
+const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9]
+
+// mencari angka yang >= 3
+
+//  with for or without filter()
+// const newAngka = []
+
+//  for (let i = 0; i < angka.length; i++) {
+//     if (angka[i] >= 3) {
+//         newAngka.push(angka[i])
+//     }
+//  }
+//  console.log(newAngka);
+
+
+// with filter()
+// const newAngka = angka.filter(a => a >= 3)
+// console.log(newAngka);
+
+
+// with map()
+// kalikan semua angka dengan 2
+// const newAngka = angka.map(a => a*2)
+// console.log(newAngka);
+
+
+// with reduce()
+// menjumlahkan seluruh element pada array
+// const newAngka = angka.reduce((accumulator, currentValue) => accumulator + currentValue , 0 )
+// // [0 + -1 + 8 + 9 + 1 + 4 + -5 + -4 + 3 + 2 + 9] *kode di atas melakukan baris ini*
+// console.log(newAngka);
+
+
+// method chaining = yang mana kita bisa menggabuungkan fungsi-fungsi pada Higher Order Function dalam satu kali eksekusi, tidak perlu disimpan ke dalam variable dulu  
+// cari angka > 5
+// lalu kalikan 3
+// dan jumlahkan
+
+const hasil = angka.filter(a => a > 5).map(a => a*3).reduce((acc, cur) => acc + cur)
+console.log(hasil);
