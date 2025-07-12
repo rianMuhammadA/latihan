ced4e28d473b0d9b7594d9a0aefa1ade19a514cd
rianMuhammadA <rianmuhammadafriansyah@gmail.com>
2025-07-12 10:01:23 +0700

    latihan callback dengan memanfaatkan open source API dan fungsi jQuery ~ WPU

---
+++ Latihan_Js_Lanjutan/callbacktrain/script.js
@@ -0,0 +1,60 @@
+// api cheapshark.com  https://www.cheapshark.com/api/1.0/deals?storeID=2&upperPrice=40&AAA=true&sortBy=recent
+// api cheapshark.com  https://www.cheapshark.com/api/1.0/games?title=finalfantasy
+$('.search-button').on('click', function(){
+    $.ajax({
+        url: 'https://www.cheapshark.com/api/1.0/games?title='+$('.input-keyword').val()+'&limit=25',
+        success: results => {
+            
+            const games = results
+            let cards = ''
+            games.forEach(res => {
+                cards += `
+                <div class="col-md-4 my-3">
+                    <div class="card" >
+                        <img src="${res.thumb}" class="card-img-top" alt="${res.external}">
+                        <div class="card-body">
+                          <h5 class="card-title">${res.external}</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">Price: ${res.cheapest}$</h6>
+                          <a href="#" class="btn btn-primary modalDetail" data-bs-toggle="modal" data-bs-target="#gamesDetail" data-gamesId="${res.gameID}">Show Details</a>
+                        </div>
+                      </div>
+                </div>
+                `
+            })
+            $('.games-container').html(cards)
+    
+    
+            // ketika tombol detail di klik
+            $('.modalDetail').on('click', function(){
+                $.ajax({
+                    url: 'https://www.cheapshark.com/api/1.0/games?id='+ $(this).data('gamesid'),
+                    success: g => {
+                        const deals = g.deals[0]
+                        const gamesDetail = `
+                        <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="${g.info.thumb}" alt="${g.info.title}" class="img-fluid">
+                    </div>
+                    <div class="col-md">
+                        <ul class="list-group">
+                            <li class="list-group-item"><h4>${g.info.title}</h4></li>
+                            <li class="list-group-item">Price: ${deals.price}</li>
+                            <li class="list-group-item">Lowest Price Ever: <strong>${g.cheapestPriceEver.price}$</strong> </li>
+                          </ul>
+                    </div>
+                </div>
+              </div>
+                        `
+                        $('.modal-body').html(gamesDetail)
+                    }
+                })
+            })
+        },
+        error: (e) =>{
+            console.log(e.responseText);
+        }
+    })
+})
+
+
